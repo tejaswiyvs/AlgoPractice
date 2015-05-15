43773ef94bbd028ababc669c66b5458262c4c565
Tejaswi <tejaswi.yvs@gmail.com>
2015-05-15 11:52:44 -0400

    init

---
+++ LeetCode/DataStructures/List.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DataStructures
+{
+    public class List
+    {
+        private Node head;
+
+        public List()
+        { 
+        
+        }
+
+        public void Add(int data) {
+            if (head == null) {
+                head = new Node(data, null);
+                return;
+            }
+
+            var node = new Node(data, head);
+            head = node;
+        }
+
+        public bool Remove(int data) {
+            if (head.Data == data) {
+                head = head.Next;
+                return false;
+            }
+
+            var node = head;
+            while (node.Next != null) {
+                if (node.Next.Data == data) {
+                    node.Next = node.Next.Next;
+                    return true;
+                }
+                node = node.Next;
+            }
+
+            return false;
+        }
+
+        public void Print() {
+            var node = head;
+            if (node == null) { Console.WriteLine("Empty list"); }
+            while (node != null) {
+                Console.Write(node.Data + " ");
+                node = node.Next;
+            }
+        }
+
+        public void Reverse() {
+            var curr = head;
+            Node prev = null;
+            var next = head.Next;
+
+            while (curr != null) {
+                curr.Next = prev;
+                prev = curr;
+                curr = next;
+                if (curr != null) {
+                    next = curr.Next;
+                }
+            }
+
+            head = prev;
+        }
+
+        public void RecursiveReverse() {
+            this.RecursiveReverse(head);
+        }
+
+        private void RecursiveReverse(Node currentNode)
+        {
+            //check for empty list 
+            if (currentNode == null)
+                return;
+
+            if (currentNode.Next == null) {
+                head = currentNode;
+                return;
+            }
+
+            RecursiveReverse(currentNode.Next);
+            currentNode.Next.Next = currentNode;
+            currentNode.Next = null;
+        }
+
+
+        private Node findTail() {
+            if (head == null) { return null; }
+
+            var node = head;
+            while (node.Next != null) {
+                node = node.Next;
+            }
+            return node;
+        }
+    }
+
+    class Node {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int Data, Node next) {
+            this.Data = Data;
+            this.Next = next;
+        }
+    }
+}
---
+++ LeetCode/DataStructures/Tree.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DataStructures
+{
+    public class Tree
+    {
+        private TreeNode root { get; set; }
+
+        public void Add(int Data) {
+            if (root == null) {
+                root = new TreeNode(Data, null, null);
+                return;
+            }
+            this.Add(root, Data);
+        }
+
+        // Builds a BST
+        private void Add(TreeNode node, int Data)
+        {
+            if (node == null) { return; }
+            if (node.Data >= Data) {
+                if (node.Left == null)
+                {
+                    node.Left = new TreeNode(Data, null, null);
+                }
+                else {
+                    this.Add(node.Left, Data);
+                }
+            }
+            else if (node.Data < Data)
+            {
+                if (node.Right == null)
+                {
+                    node.Right = new TreeNode(Data, null, null);
+                }
+                else
+                {
+                    this.Add(node.Right, Data);
+                }
+            }
+        }
+
+        // Assumes BST and removes
+        public void Remove(int Data) {
+            this.Remove(root, Data);
+        }
+
+        private void Remove(TreeNode node, int Data) {
+            if (node == null) {
+                return;
+            }
+
+            if (node.Left != null && node.Left.Data == Data && node.Left.isLeaf()) {
+                node.Left = null;
+            }
+            else if (node.Left != null && node.Left.Data == Data && !node.Left.isLeaf()) { 
+                // Replace node.left with the right most node in the left subtree
+            }
+            else if (node.Right != null && node.Right.Data == Data && !node.Right.isLeaf())
+            {
+                node.Right = null;
+            }
+            else if (node.Right != null && node.Right.Data == Data && !node.Right.isLeaf())
+            {
+                // Replace node.right with the left most node in the right subtree
+            }
+        }
+
+        // Should probably be an enum instead, but whatever
+        public void traverse(string type) {
+            if (type == "inorder") {
+                this.printInOrderTraversal(root);
+            }
+            else if (type == "preorder") {
+                this.printPreOrderTraversal(root);
+            }
+            else if (type == "postorder") {
+                this.printPostOrderTraversal(root);
+            }
+            return;
+        }
+
+        private void printInOrderTraversal(TreeNode node) {
+            if (node == null) { return;  }
+
+            this.printInOrderTraversal(node.Left);
+            Console.Write(node.Data + " ");
+            this.printInOrderTraversal(node.Right);
+        }
+
+        private void printPreOrderTraversal(TreeNode node)
+        {
+            if (node == null) { return; }
+
+            Console.Write(node.Data + " ");
+            this.printInOrderTraversal(node.Left);
+            this.printInOrderTraversal(node.Right);
+        }
+
+        private void printPostOrderTraversal(TreeNode node)
+        {
+            if (node == null) { return; }
+
+            this.printInOrderTraversal(node.Left);
+            this.printInOrderTraversal(node.Right);
+            Console.Write(node.Data + " ");
+        }
+    }
+
+    class TreeNode
+    {
+        public int Data { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode()
+        { 
+        
+        }
+
+        public TreeNode(int data, TreeNode left, TreeNode right)
+        {
+            this.Data = data;
+            this.Left = left;
+            this.Right = right;
+        }
+
+        public bool isLeaf() 
+        { 
+            return (this.Left == null && this.Right == null);
+        }
+    }
+}
